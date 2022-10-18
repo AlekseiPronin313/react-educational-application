c31bda049db77808f3383d3eeb19aeaa9a524377
Pronin <isw.pronin@yandex.ru>
2022-10-18 20:08:46 +0500

    test

---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () =>{
+    return (
+        <header className='header'>
+            <img className='header_img' src='https://img.icons8.com/cute-clipart/344/free-fire.png' alt='img_header'/>
+        </header>
+    )
+}
+
+export default Header
