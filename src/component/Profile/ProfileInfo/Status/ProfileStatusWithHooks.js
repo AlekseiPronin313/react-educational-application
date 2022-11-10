b39a31467a97fc1a36ad3d7e660a2e5adc150f57
Pronin <isw.pronin@yandex.ru>
2022-11-10 15:15:39 +0500

    update status component

--- src/component/Profile/ProfileInfo/Status/ProfileStatusWithHooks.js
+++ src/component/Profile/ProfileInfo/Status/ProfileStatusWithHooks.js
@@ -1,10 +1,14 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Style from './ProfileStatus.module.css'
 
 const ProfileStatusWithHook = (props) => {
 
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
 
     const activateEditMode = () => {
         setEditMode(true)
