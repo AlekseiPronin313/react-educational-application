1b9b7a14c1b7810a3a48d9a470223b1a75b7694b
Pronin <isw.pronin@yandex.ru>
2023-02-09 21:44:48 +0500

    type HeaderContainer

---
+++ src/component/Header/HeaderContainer.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Header from "./Header";
+import {connect} from "react-redux";
+import { logout} from "../../redux/auth-reducer";
+import {AppStateType} from "../../redux/redux-store";
+
+type MapStatePropsType = {
+    isAuth: boolean
+    login: null | string
+}
+
+type MapDispatchPropsType = {
+
+}
+
+type OwnPropsType = {
+
+}
+
+type PropsType = MapStatePropsType | MapDispatchPropsType | OwnPropsType
+
+class HeaderContainer extends React.Component<PropsType>{
+    render() {
+        return (
+            <Header {...this.props}/>
+        )
+    }
+}
+
+const mapStateToProps = (state: AppStateType): MapStatePropsType => ({
+    isAuth: state.auth.isAuth,
+    login: state.auth.login
+})
+
+export default connect<MapStatePropsType, MapDispatchPropsType, OwnPropsType, AppStateType>(mapStateToProps, {logout}) (HeaderContainer)
