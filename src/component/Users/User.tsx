e914b408d2c935d7c1b8f0ad316843e5eb6ae96e
Pronin <isw.pronin@yandex.ru>
2023-02-14 21:46:35 +0500

    type component User

--- src/component/Users/User.tsx
+++ src/component/Users/User.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import Style from './Users.module.scss'
 import img_avatar from './../../assets/image/img_avatar.png'
 import {NavLink} from "react-router-dom";
+import {UserType} from "../../types/types";
 
-const User = ({user, followingInProgress, unfollow, follow}) => {
+type PropsType = {
+    user: UserType
+    followingInProgress: Array<number>
+    unfollow: (userId: number) => void
+    follow: (userId: number) => void
+}
+
+const User: React.FC<PropsType> = ({user, followingInProgress, unfollow, follow}) => {
     return (
         <div className={Style.users_box}>
             <NavLink className={Style.link} to={'/profile/' + user.id}>
