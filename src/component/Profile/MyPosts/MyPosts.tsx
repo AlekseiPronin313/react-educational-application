7e99ee6e9f1f69ff213cd2ac846dfedcfb5b5f2b
Pronin <isw.pronin@yandex.ru>
2023-02-15 15:37:22 +0500

    type component MyPosts

--- src/component/Profile/MyPosts/MyPosts.tsx
+++ src/component/Profile/MyPosts/MyPosts.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Style from './MyPosts.module.scss'
 import Post from "./Posts/Post";
-import MyPostsReduxForm from "./MyPostsForm";
+import MyPostsReduxForm, {AddPostFormValuesType} from "./MyPostsForm";
+import {PostType} from "../../../types/types";
 
-const MyPosts = React.memo(props => {
-    const addNewPosts = (values) => {
+type PropsType = {
+    profilePage: Array<PostType>
+    addPost: (posts: string) => void
+}
+
+const MyPosts: React.FC<PropsType> = React.memo(props => {
+    const addNewPosts = (values: AddPostFormValuesType) => {
         props.addPost(values.posts)
     }
 
